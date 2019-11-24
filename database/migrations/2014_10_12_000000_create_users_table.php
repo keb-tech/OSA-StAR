afaff7341263d8c5cf90caba79739cd6faa98515
kylie777 <kyliejanechua@gmail.com>
2019-11-24 22:27:06 +0800

    Update 2014_10_12_000000_create_users_table.php
    Added "Organization_Name" in Create Users Migration table coz keb-tech is an idiot and forgot to put a crucial column in it.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('student_number')->nullable()->unique();
+            $table->string('organization_name')->nullable()->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
