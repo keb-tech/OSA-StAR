ed3246f2b780caba8a6eb0b7cb9b2aadb4e2f6eb
Keb-tech <kael.gtx@gmail.com>
2019-12-10 00:27:20 +0800

    Set fake student list for custom seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,12 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
         $this->call(RoleTableSeeder::class);
         $this->call(UserTableSeeder::class);
-        $this->call(EventTableSeeder::class);
+        // $this->call(EventTableSeeder::class);
         $this->call(StudentTableSeeder::class);
-        $this->call(SpeakerTableSeeder::class);
+        // $this->call(SpeakerTableSeeder::class);
         $this->call(CollegesTableSeeder::class);
         $this->call(CollegeCoursesTableSeeder::class);
     }
--- database/seeds/StudentTableSeeder.php
+++ database/seeds/StudentTableSeeder.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Event;
+// use App\Event;
 use App\Student;
-use Faker\Generator as Faker;
+// use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 
 class StudentTableSeeder extends Seeder
@@ -14,11 +14,149 @@ class StudentTableSeeder extends Seeder
      */
     public function run()
     {
-        Event::all()->each(function($event) {
-            $involvement_types = ['Participant', 'Organizer'];
-            factory(Student::class, 5)->create()->each(function($student) use ($event, $involvement_types) {
-                $event->students()->attach($student->id, ['involvement' => $involvement_types[array_rand($involvement_types)]]);
-            });
-        });
+        // Event::all()->each(function($event) {
+        //     $involvement_types = ['Participant', 'Organizer'];
+        //     factory(Student::class, 5)->create()->each(function($student) use ($event, $involvement_types) {
+        //         $event->students()->attach($student->id, ['involvement' => $involvement_types[array_rand($involvement_types)]]);
+        //     });
+        // });
+
+        
+        Student::create([
+            'student_number' => '2015093214',
+            'first_name' => 'Kael',
+            'last_name' => 'Arkangel',
+            'middle_initial' => 'S',
+            'college' => 'Faculty of Engineering',
+            'course' => 'Bachelor of Science in Civil Engineering'
+        ]);
+
+        Student::create([
+            'student_number' => '2015081923',
+            'first_name' => 'Elika',
+            'last_name' => 'Celestial',
+            'middle_initial' => 'T',
+            'college' => 'College of Architecture',
+            'course' => 'Bachelor of Science in Architecture'
+        ]);
+
+        Student::create([
+            'student_number' => '2015077126',
+            'first_name' => 'Roi Geralt',
+            'last_name' => 'Regalla',
+            'middle_initial' => 'B',
+            'college' => 'Faculty of Civil Law',
+            'course' => 'Bachelor of Laws'
+        ]);
+
+        Student::create([
+            'student_number' => '2015077127',
+            'first_name' => 'Reinessia Lucille',
+            'last_name' => 'Regalla',
+            'middle_initial' => 'B',
+            'college' => 'Faculty of Engineering',
+            'course' => 'Bachelor of Science in Civil Engineering'
+        ]);
+
+        Student::create([
+            'student_number' => '2016173521',
+            'first_name' => 'Sayuri',
+            'last_name' => 'Haruno',
+            'middle_initial' => 'P',
+            'college' => 'College of Architecture',
+            'course' => 'Bachelor of Science in Architecture'
+        ]);
+
+        Student::create([
+            'student_number' => '2015091919',
+            'first_name' => 'Kuku',
+            'last_name' => 'Rikapu',
+            'middle_initial' => 'O',
+            'college' => 'Institute of Information and Computing Sciences',
+            'course' => 'Bachelor of Science in Information Technology'
+        ]);
+
+        Student::create([
+            'student_number' => '2015012345',
+            'first_name' => 'Kimmo',
+            'last_name' => 'Lascove',
+            'middle_initial' => 'D',
+            'college' => 'Faculty of Engineering',
+            'course' => 'Bachelor of Science in Mechanical Engineering'
+        ]);
+
+        Student::create([
+            'student_number' => '2015061916',
+            'first_name' => 'Nayr',
+            'last_name' => 'Matianlad',
+            'middle_initial' => 'D',
+            'college' => 'Institute of Information and Computing Sciences',
+            'course' => 'Bachelor of Science in Computer Science'
+        ]);
+
+        Student::create([
+            'student_number' => '2015014014',
+            'first_name' => 'Lesnar',
+            'last_name' => 'Quezon',
+            'middle_initial' => 'E',
+            'college' => 'College of Rehabilitation Sciences',
+            'course' => 'Bachelor of Science in Physical Therapy'
+        ]);
+
+        Student::create([
+            'student_number' => '2015076076',
+            'first_name' => 'Charlie',
+            'last_name' => 'Benedictus',
+            'middle_initial' => 'C',
+            'college' => 'Institute of Physical Education and Athletics',
+            'course' => 'Bachelor of Physical Education, major in Sports Wellness & Management'
+        ]);
+
+        Student::create([
+            'student_number' => '2015027827',
+            'first_name' => 'Augustus',
+            'last_name' => 'Pogititus',
+            'middle_initial' => 'E',
+            'college' => 'College of Nursing',
+            'course' => 'Bachelor of Science in Nursing'
+        ]);
+
+        Student::create([
+            'student_number' => '2015021120',
+            'first_name' => 'Jojo',
+            'last_name' => 'Jolens',
+            'middle_initial' => 'J',
+            'college' => 'Conservatory of Music',
+            'course' => 'Bachelor of Music in Vocal Performance'
+        ]);
+
+        Student::create([
+            'student_number' => '2015077777',
+            'first_name' => 'Hyper',
+            'last_name' => 'Canins',
+            'middle_initial' => 'D',
+            'college' => 'College of Tourism and Hospitality Management',
+            'course' => 'Bachelor of Science in Hospitality Management, major in Culinary Entrepreneurship'
+        ]);
+
+        Student::create([
+            'student_number' => '2015087000',
+            'first_name' => 'Bbibbi',
+            'last_name' => 'Mike',
+            'middle_initial' => 'U',
+            'college' => 'Conservatory of Music',
+            'course' => 'Bachelor of Music in Vocal Performance'
+        ]);
+
+        Student::create([
+            'student_number' => '2015021011',
+            'first_name' => 'Olivia',
+            'last_name' => 'Dampa',
+            'middle_initial' => 'I',
+            'college' => 'Conservatory of Education',
+            'course' => 'Bachelor of Science and Food Technology'
+        ]);
+
+
     }
 }
